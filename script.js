962ed687aa18d4656e8f70602d64a9678b8a6376
Sandra <sandra0924@protonmail.com>
2025-08-19 10:55:15 +0200

    removed html2 and added upload code and script to regular indexfile

--- script.js
+++ script.js
@@ -2,80 +2,80 @@
 import { supabase } from "./supabase.js";
 
 // --- Painting on canvas ---
-// const canvas = document.getElementById("drawing-board");
-// const toolbar = document.getElementById("toolbar");
-// const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("drawing-board");
+const toolbar = document.getElementById("toolbar");
+const ctx = canvas.getContext("2d");
 
-// const canvasOffsetX = canvas.offsetLeft;
-// const canvasOffsetY = canvas.offsetTop;
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
 
-// canvas.width = window.innerWidth - canvasOffsetX;
-// canvas.height = window.innerHeight - canvasOffsetY;
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
 
-// let isPainting = false;
-// let lineWidth = 5;
+let isPainting = false;
+let lineWidth = 5;
 
-// function getXY(e) {
-//   if (e.touches) {
-//     return {
-//       x: e.touches[0].clientX - canvasOffsetX,
-//       y: e.touches[0].clientY - canvasOffsetY,
-//     };
-//   } else {
-//     return {
-//       x: e.clientX - canvasOffsetX,
-//       y: e.clientY - canvasOffsetY,
-//     };
-//   }
-// }
+function getXY(e) {
+  if (e.touches) {
+    return {
+      x: e.touches[0].clientX - canvasOffsetX,
+      y: e.touches[0].clientY - canvasOffsetY,
+    };
+  } else {
+    return {
+      x: e.clientX - canvasOffsetX,
+      y: e.clientY - canvasOffsetY,
+    };
+  }
+}
 
-// toolbar.addEventListener("click", (e) => {
-//   if (e.target.id === "clear") {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   }
-// });
+toolbar.addEventListener("click", (e) => {
+  if (e.target.id === "clear") {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+});
 
-// toolbar.addEventListener("change", (e) => {
-//   if (e.target.id === "stroke") {
-//     ctx.strokeStyle = e.target.value;
-//   }
-//   if (e.target.id === "lineWidth") {
-//     lineWidth = e.target.value;
-//   }
-// });
+toolbar.addEventListener("change", (e) => {
+  if (e.target.id === "stroke") {
+    ctx.strokeStyle = e.target.value;
+  }
+  if (e.target.id === "lineWidth") {
+    lineWidth = e.target.value;
+  }
+});
 
-// function draw(e) {
-//   if (!isPainting) return;
-//   e.preventDefault();
+function draw(e) {
+  if (!isPainting) return;
+  e.preventDefault();
 
-//   const { x, y } = getXY(e);
+  const { x, y } = getXY(e);
 
-//   ctx.lineWidth = lineWidth;
-//   ctx.lineCap = "round";
-//   ctx.lineTo(x, y);
-//   ctx.stroke();
-// }
+  ctx.lineWidth = lineWidth;
+  ctx.lineCap = "round";
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
 
-// function startPainting(e) {
-//   isPainting = true;
-//   const { x, y } = getXY(e);
-//   ctx.beginPath();
-//   ctx.moveTo(x, y);
-// }
+function startPainting(e) {
+  isPainting = true;
+  const { x, y } = getXY(e);
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+}
 
-// function stopPainting() {
-//   isPainting = false;
-//   ctx.stroke();
-//   ctx.beginPath();
-// }
+function stopPainting() {
+  isPainting = false;
+  ctx.stroke();
+  ctx.beginPath();
+}
 
-// canvas.addEventListener("mousedown", startPainting);
-// canvas.addEventListener("mouseup", stopPainting);
-// canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", stopPainting);
+canvas.addEventListener("mousemove", draw);
 
-// canvas.addEventListener("touchstart", startPainting);
-// canvas.addEventListener("touchend", stopPainting);
-// canvas.addEventListener("touchmove", draw);
+canvas.addEventListener("touchstart", startPainting);
+canvas.addEventListener("touchend", stopPainting);
+canvas.addEventListener("touchmove", draw);
 
 // --- Upload painting ---
 const form = document.getElementById("upload-form");
