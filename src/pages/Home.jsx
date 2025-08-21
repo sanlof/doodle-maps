105cefdca26598828ee852ce1f60e6e84207005e
filiplyrheden <filip.lyrheden@gmail.com>
2025-08-21 14:50:40 +0200

    refactor: update Home component layout and styling; add how-to section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,28 @@ export default function Home() {
 
   return (
     <div className="container">
-      <nav>
-        <li>Gallery</li>
-        <li>FAQ</li>
-        <li>Help</li>
-      </nav>
       <img src="/images/doodlemaps.png" alt="Doodle Maps logo" />
-      <button onClick={goToMap}>Play</button>
+      <div className="how-to">
+        <h2 className="how-to-heading">How to play:</h2>
+        <p className="how-to-text">
+          1: Pick a Spot! The Map shows several Doodle Spots that refresh every
+          4 hours.
+        </p>
+        <p className="how-to-text">
+          2: Time to Doodle! Upon arrival, check out your surroundings and draw
+          what inspires you!
+        </p>
+        <p className="how-to-text">
+          3: Save it! Done with your masterpiece? You are ink-credible! Tap Save
+          to share your artwork to the gallery.
+        </p>
+      </div>
+      <div className="home-buttons">
+        <button className="button">Gallery</button>
+        <button onClick={goToMap} className="button">
+          Play
+        </button>
+      </div>
     </div>
   );
 }
