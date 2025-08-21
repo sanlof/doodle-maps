63a02a622e94f46d65c62ca329a65ddab28d6e84
Sandra <sandra0924@protonmail.com>
2025-08-21 11:27:17 +0200

    added global container for all pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <div class="container">
+    <App />
+  </div>
+);
